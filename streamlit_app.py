297eb78e51da968d020a929af9b06236ab44d8a4
Camila Rinaldi <camiivelleda@gmail.com>
2025-07-16 15:40:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1091,14 +1091,17 @@ with aba_qualifica:
     municipios_validos = sorted(municipios_crs_resultados)
     # SelectBox para município
     municipality = st.selectbox("Selecione o município", options=municipios_validos)
-    # Filtro de ano
-    ano_escolhido = st.text_input("Digite o ano desejado (ex: 2025):")
+    # Filtra os dados do município
+    dados_municipio = dados[dados['municipality'] == municipality]
+    
+    # Filtra apenas os anos com coletas (eggs > 0)
+    anos_disponiveis = dados_municipio[dados_municipio['eggs'] > 0]['year'].dropna().unique()
+    anos_disponiveis = sorted(anos_disponiveis)
+    ano_escolhido = st.selectbox("Selecione o ano disponível com coletas", options=anos_disponiveis)
+ 
     # Botão
     processar = st.button("Processar")
-st.write(f"CRS selecionada: {crs_selecionada}")
-st.write("Municípios encontrados:", municipios_validos)
-st.write("df_resultados CRS únicos:", df_resultados['crs'].unique())
-
+  
  if processar:
      #df = get_last_counting_public(municipality)
      df_pre_filtro = dados[['counting_id', 'date', 'date_collect', 'eggs', 'latitude', 'longitude',
