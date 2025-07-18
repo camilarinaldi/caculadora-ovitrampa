078fef80e706b759e71ddb6ed4960eb032eed685
Camila Rinaldi <camiivelleda@gmail.com>
2025-07-18 15:28:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1101,7 +1101,7 @@ with aba_qualifica:
        'state_code', 'state_name', 'time', 'week', 'year']]
 
     # Filtrando o municipio e o ano
-     filtro = (df_pre_filtro['municipality'] == municipio) & (df_pre_filtro['year'] == int(ano))
+     filtro = (df_pre_filtro['municipality'] == municipio) & (df_pre_filtro['year'] == int(ano_escolhido))
      df = df_pre_filtro[filtro]
      
      # Garantir que a coluna 'date' esteja em formato datetime
